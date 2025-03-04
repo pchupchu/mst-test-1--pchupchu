3121ef76cb4cfe5065473817887d37d8f068e6ca
Marina <pchupchu@yandex.ru>
2025-03-04 14:36:28 +0300

    Add mobile and tablet screens

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function initMenuToggle() {
 }
 
 document.addEventListener("DOMContentLoaded", initMenuToggle);
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("header_active"); 
+  } else {
+    header.classList.remove("header_active"); 
+  }
+});
